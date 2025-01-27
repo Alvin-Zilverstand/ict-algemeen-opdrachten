5e0e0a7907fee55389f89acc2c73bf9b341ddcaa
vista-man <524715@vistacollege.nl>
2025-01-27 19:32:46 +0100

    Remove obsolete cache and configuration files for .NET versions 5.0, 6.0, and 9.0

--- Csharp/Bankrekening/obj/Debug/net9.0-windows/Bankrekening.AssemblyInfo.cs
+++ Csharp/Bankrekening/obj/Debug/net9.0-windows/Bankrekening.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bankrekening")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1de1230437a6adbf29a45e54436a13fd85317a76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d79eacbc2afcd0aa17aefc524f3f8f423dc30634")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bankrekening")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bankrekening")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
