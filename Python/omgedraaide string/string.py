aecf610e9c643e3450e520e5136199a9fd82d374
vista-man <524715@vistacollege.nl>
2025-01-28 13:39:40 +0100

    Refactor user prompts and remove unnecessary comments in string reversal and palindrome scripts

--- Python/omgedraaide string/string.py
+++ Python/omgedraaide string/string.py
@@ -1,7 +1,6 @@
 import time
 
-# Vraag de gebruiker om een string in te voeren
 input_string = input("Voer een string in: ")
-# Print de omgekeerde string
-print(input_string[::-1])  # Output: "zeeD"
+
+print(input_string[::-1])
 time.sleep(2)
--- Python/palindroom/palindroom.py
+++ Python/palindroom/palindroom.py
@@ -1,11 +1,10 @@
 def is_palindrome(word):
-    # Controleer of een woord een palindroom is
     return word == word[::-1]
 
-# Vraag de gebruiker om een woord in te voeren
+
 input_word = input("Voer een woord in: ")
 
-# Controleer of het woord een palindroom is en geef het resultaat weer
+
 if is_palindrome(input_word):
     print(f"{input_word} is een palindroom.")
 else:
