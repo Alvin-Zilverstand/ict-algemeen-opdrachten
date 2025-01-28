d9d36fdc60c60ae9c35165903a637a145aa18846
vista-man <524715@vistacollege.nl>
2025-01-28 13:54:52 +0100

    Remove unnecessary blank lines in palindrome check script

--- Python/palindroom/palindroom.py
+++ Python/palindroom/palindroom.py
@@ -1,9 +1,7 @@
 def is_palindrome(word):
     return word == word[::-1]
 
-
 input_word = input("Voer een woord in: ")
-
 
 if is_palindrome(input_word):
     print(f"{input_word} is een palindroom.")
