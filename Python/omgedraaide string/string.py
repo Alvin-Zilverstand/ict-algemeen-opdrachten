d5155c48b3bb3ae83f1439d1dc7d57621e0bd05d
vista-man <524715@vistacollege.nl>
2025-01-28 13:34:27 +0100

    Add user input for string reversal and delay before output

--- Python/omgedraaide string/string.py
+++ Python/omgedraaide string/string.py
@@ -1,2 +1,7 @@
+import time
+
+# Vraag de gebruiker om een string in te voeren
+input_string = input("Voer een string in: ")
 # Print de omgekeerde string
-print("Deez"[::-1])  # Output: "zeeD"
+print(input_string[::-1])  # Output: "zeeD"
+time.sleep(2)
